c5afc6aad2612acdfefc20334d62a85a7c5b496a
BushidoToken <59974887+BushidoUK@users.noreply.github.com>
2024-03-16 21:38:25 +0000

    Update RetrieveHTML.py

--- RetrieveHTML.py
+++ RetrieveHTML.py
@@ -1,6 +1,10 @@
 import requests
 
-URL = "<ENTER URL>" # Target URL to fetch HTML from
+URL = "<TARGET URL>" # Target URL to fetch HTML from
 page = requests.get(URL) # Performs HTTP GET Request
 
-print(page.text) # Displays HTML in the Terminal
+# Write the page content to a file called "TargetSiteHTML.txt"
+with open("TargetSiteHTML.txt", "w") as file:
+    file.write(page.text)
+
+print(f"Page content saved to TargetSiteHTML.txt")
